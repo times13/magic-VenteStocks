16b1707fe4e1a0b132e299bc6206501e9d6c1bbe
Times <alfred.times@yahoo.fr>
2026-05-04 10:29:16 -0400

    Ajout du module backend au projet

---
+++ backend/src/test/java/ht/fds/mbds/alfred/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ht.fds.mbds.alfred.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
